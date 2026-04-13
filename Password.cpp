8a8183a120dbe08231046d67d29b22ed1305253a
zigglethorp <cshansen@csuchico.edu>
2026-04-13 12:56:34 -0700

    Initial commit

--- Password.cpp
+++ Password.cpp
@@ -18,11 +18,106 @@ int Password::count_leading_characters(string phrase){
   }
   return repetition;
 }
+//////REPLACE BELOW
+  /*receives a string and returns whether it has both at least one upper-case
+  letter and at least one lower-case letter
+  */
+ bool Password::has_mixed_case(string pass){
+   return false;
+ }
+
+// Implementation A  Pass
+#include <cctype>
+bool Password::has_mixed_case(string str)
+{
+  bool has_lower = false;
+  bool has_upper = false;
+  for (char ch : str)
+  {
+    if (std::islower(ch))
+    {
+      has_lower = true;
+    }
+    else if (std::isupper(ch))
+    {
+      has_upper = true;
+    }
+  }
+  return has_lower && has_upper;
+}
+*/
+/*
+// Implementation B  Pass
+bool Password::has_mixed_case(string str)
+{
+  int count_lower = 0;
+  for (int i = 0; i < str.length(); i++)
+  {
+    if (str[i] >= 'a' && str[i] <= 'z')
+    {
+      count_lower++;
+    }
+  }
+  return count_lower > 0 && count_lower < str.length();
+}
+  */
 
 /*
-  receives a string and returns whether it has both at least one upper-case
-  letter and at least one lower-case letter
+// Implementation C   Pass
+bool Password::has_mixed_case(string str)
+{
+  bool lower = false;
+  bool upper = false;
+  for (int i = 0; i < str.length(); i++)
+  {
+    if (str[i] >= 'a' && str[i] <= 'z')
+    {
+      lower = true;
+    }
+    else if (str[i] >= 'A' && str[i] <= 'Z')
+    {
+      upper = true;
+    }
+  }
+  return upper && lower;
+}
 */
-bool Password::has_mixed_case(string pass){
+
+/*
+// Implementation D   PASS
+#include <cctype>
+bool Password::has_mixed_case(string str)
+{
+  bool is_lower = false;
+  bool is_upper = false;
+  for (char c : str)
+  {
+    if (std::islower(c))
+    {
+      is_lower = true;
+    }
+    else
+    {
+      is_upper = true;
+    }
+  }
+  return is_lower && is_upper;
+}
+  */
+/*
+  // Implementation E
+bool Password::has_mixed_case(string str)
+{
+  bool found = false;
+  for(char c : str){
+    if( !found && c >= 'A' && c <= 'Z' ){
+      found = true;
+    }
+    else if( found && c >= 'a' && c <= 'z'){
+      return true;
+    }
+  }
   return false;
 }
+
+*/
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -14,9 +14,51 @@ class PasswordTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PasswordTest, single_letter_password)
+TEST(PasswordTest, has_mixed_cases)
 {
 	Password my_password;
 	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1, actual);
+	ASSERT_EQ(1, my_password.count_leading_characters("Z"));   //// This makes sure the value that is given is what is expected. 
 }
+
+TEST(PasswordTest1, single_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("ZzZ");
+	ASSERT_EQ(2, my_password.count_leading_characters("ZzZ"));   //// This makes sure the value that is given is what is expected. 
+}
+
+TEST(PasswordTest2, single_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("   ");
+	ASSERT_EQ(2, my_password.count_leading_characters("   "));   //// This makes sure the value that is given is what is expected. 
+}
+
+TEST(PasswordTest3, single_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("""I");
+	ASSERT_EQ(2, my_password.count_leading_characters("""I"));   //// This makes sure the value that is given is what is expected. 
+}
+
+TEST(PasswordTest4, single_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("Aab");
+	ASSERT_EQ(1, my_password.count_leading_characters("Aab"));   //// This makes sure the value that is given is what is expected. 
+}
+
+TEST(PasswordTest5, single_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("AAa");
+	ASSERT_EQ(2, my_password.count_leading_characters("AAa"));   //// This makes sure the value that is given is what is expected. 
+}
+
+TEST(PasswordTest6, single_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("AA ");
+	ASSERT_EQ(2, my_password.count_leading_characters("AA "));   //// This makes sure the value that is given is what is expected. 
+};
